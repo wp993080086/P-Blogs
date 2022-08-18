3b2315f5883a35748c4342bd65749519d904c0d0
pdd993080086 <993080086@qq.com>
2022-08-18 18:24:12 +0800

    修改菜单头；

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,29 @@
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import styles from './header.module.scss'
 import Logo from '@/static/images/logo.png'
 
 export default defineComponent({
   name: 'pHeader',
   setup() {
+    const menu = reactive([
+      { title: '个人主页', id: '001' },
+      { title: '前端工具', id: '002' },
+      { title: '文章合集', id: '003' }
+    ])
     return () => (
       <div id={styles.header_box}>
-        <img src={Logo} alt="logo" class={styles.logo} />
+        <div class={styles.header_main}>
+          <div class={styles.logo_box}>
+            <img src={Logo} alt="logo" class={styles.logo} />
+          </div>
+          <div class={styles.menu_box}>
+            {
+              menu.map(item => {
+                return <div class={styles.menu_item}>{item.title}</div>
+              })
+            }
+          </div>
+        </div>
       </div>
     )
   }
