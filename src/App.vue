2c0dc52db350bccd57f0867ef7498e885733a424
pdd993080086 <993080086@qq.com>
2022-08-24 15:04:33 +0800

    添加样式；

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,20 @@ import Header from '@/components/header/header'
 </script>
 
 <template>
-  <Header />
-    <div id="main_box">
-      <el-scrollbar>
-        <router-view v-slot="{ Component }">
-          <transition name="fade" mode="out-in">
-            <component :is="Component" />
-          </transition>
-        </router-view>
-      </el-scrollbar>
-    </div>
+  <div id="main_box">
+    <Header />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 #main_box {
+  position: relative;
   width: 100%;
-  height: calc(100% - 45px);
 }
 </style>
 
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, reactive, onMounted } from 'vue'
+import { debounce } from '@/utils/index'
 import styles from './header.module.scss'
 import Logo from '@/static/images/logo.png'
 
@@ -10,6 +11,19 @@ export default defineComponent({
       { title: '前端工具', id: '002' },
       { title: '文章合集', id: '003' }
     ])
+    // 滚动监听
+    const handleScroll = (target: Event) => {
+      const dom = document.querySelector('#main_box') as HTMLElement
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+      const offsetTop = dom.offsetTop // 获取所需元素到文档顶部的距离，然后判断滚动条位置让该元素是否固定定位
+      console.log(scrollTop)
+      console.log(offsetTop)
+    }
+    // 挂载之后
+    onMounted(() => {
+      const fn = debounce(50, handleScroll)
+      window.addEventListener('scroll', fn, true)
+    })
     return () => (
       <div id={styles.header_box}>
         <div class={styles.header_main}>
